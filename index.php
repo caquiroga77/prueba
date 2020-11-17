f4525cc431c62837cee9a83fa6c50ec3296fac61
Camilo Quiroga <74612876+caquiroga77@users.noreply.github.com>
2020-11-17 14:41:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+<div class="row">
+<h3>My CRUD PHP grid</h3>
+</div>
+<div class="row">
+<table class="table table-striped table-bordered">
+<thead>
+  <tr>
+    <th>nombre</th>
+    <th>email</th>
+    <th>celular</th>
+  </tr>
+</thead>
+<tbody>
+<?php
+include 'database.php';
+$pdo = Database::connect();
+$sql = 'SELECT * FROM customers ORDER BY id DESC';
+foreach ($pdo->query($sql) as $row) {
+echo '<tr>';
+echo '<td>' . $row['name'] . '</td>';
+echo '<td>' . $row['email'] . '</td>';
+echo '<td>' . $row['mobile'] . '</td>';
+echo '</tr>';
+}
+Database::disconnect();
+?>
+</tbody>
+</table>
+</div>
+</div> <!-- /container -->
+</body>
+</html>
+
+<p>
+  <a href="create.php" class="btn btn-success">Create</a>
+</p>
